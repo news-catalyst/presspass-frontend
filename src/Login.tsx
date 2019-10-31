ed162db4e3b69530107fd80877b08ab1badb757e
R. Miles McCain <x1-key@sendmiles.email>
2019-10-31 14:13:35 -0400

    Start login component

--- src/Login.tsx
+++ src/Login.tsx
@@ -1,12 +1,29 @@
-import React from 'react';
+import React, { useState, SyntheticEvent } from 'react';
+
+class LoginCredentials {
+  constructor(private username: string, private password: string) { }
+
+  login() {
+    console.log("login request");
+  }
+}
 
 const Login: React.FC = () => {
+  let [username, setUsername] = useState("");
+  let [password, setPassword] = useState("");
+
+  let formSubmit = (event: SyntheticEvent) => {
+    event.preventDefault();
+    new LoginCredentials(username, password).login();
+  }
+
   return (
-    <div>
+    <form onSubmit={formSubmit}>
       <h1>Login</h1>
-      <input type="text" placeholder="Username" name="username" />
-      <input type="password" placeholder="Password" name="password" />
-    </div>
+      <input type="text" placeholder="Username" name="username" value={username} onChange={event => setUsername(event.target.value)} />
+      <input type="password" placeholder="Password" name="password" value={password} onChange={event => setPassword(event.target.value)} />
+      <button type="submit">Login</button>
+    </form>
   );
 }
 
