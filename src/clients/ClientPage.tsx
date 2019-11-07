0b9d83019248551a50d2c6dd9a8fea43ace9d6b8
R. Miles McCain <x1-key@sendmiles.email>
2019-11-07 15:11:17 -0500

    Add table body to client datatable

--- src/clients/ClientPage.tsx
+++ src/clients/ClientPage.tsx
@@ -16,8 +16,8 @@ const ClientPage = (props: ClientPageProps) => {
     ensureClients(props.actions, props.clients);
   }, [props.actions])
 
-  if(!props.clients.hydrated) {
-    return <LoadingPlaceholder/>;
+  if (!props.clients.hydrated) {
+    return <LoadingPlaceholder />;
   }
 
   let client = props.clients.clients[props.client];
@@ -27,94 +27,96 @@ const ClientPage = (props: ClientPageProps) => {
       <p className="subtitle">OpenID Client</p>
       <h1 className="title is-size-1">{client.name}</h1>
       <table className="table">
-        <tr>
-          <td className="has-text-weight-bold">
-            Name
+        <tbody>
+          <tr>
+            <td className="has-text-weight-bold">
+              Name
           </td>
-          <td>
-            {client.name}
+            <td>
+              {client.name}
+            </td>
+          </tr>
+          <tr>
+            <td className="has-text-weight-bold">
+              OpenID Client Type
           </td>
-        </tr>
-        <tr>
-          <td className="has-text-weight-bold">
-            OpenID Client Type
+            <td>
+              {client.client_type}
+            </td>
+          </tr>
+          <tr>
+            <td className="has-text-weight-bold">
+              OpenID Client ID
           </td>
-          <td>
-            {client.client_type}
+            <td>
+              {client.client_id}
+            </td>
+          </tr>
+          <tr>
+            <td className="has-text-weight-bold">
+              OpenID Client Secret
           </td>
-        </tr>
-        <tr>
-          <td className="has-text-weight-bold">
-            OpenID Client ID
+            <td>
+              {client.client_secret}
+            </td>
+          </tr>
+          <tr>
+            <td className="has-text-weight-bold">
+              Created
           </td>
-          <td>
-            {client.client_id}
+            <td>
+              {client.date_created}
+            </td>
+          </tr>
+          <tr>
+            <td className="has-text-weight-bold">
+              Website URL
           </td>
-        </tr>
-        <tr>
-          <td className="has-text-weight-bold">
-            OpenID Client Secret
+            <td>
+              {client.website_url}
+            </td>
+          </tr>
+          <tr>
+            <td className="has-text-weight-bold">
+              Contact Email
           </td>
-          <td>
-            {client.client_secret}
+            <td>
+              {client.contact_email}
+            </td>
+          </tr>
+          <tr>
+            <td className="has-text-weight-bold">
+              Logo
           </td>
-        </tr>
-        <tr>
-          <td className="has-text-weight-bold">
-            Created
+            <td>
+              {client.logo}
+            </td>
+          </tr>
+          <tr>
+            <td className="has-text-weight-bold">
+              Reuse Content
           </td>
-          <td>
-            {client.date_created}
+            <td>
+              {client.reuse_content}
+            </td>
+          </tr>
+          <tr>
+            <td className="has-text-weight-bold">
+              Redirect URIs
           </td>
-        </tr>
-        <tr>
-          <td className="has-text-weight-bold">
-            Website URL
+            <td>
+              {client.redirect_uris}
+            </td>
+          </tr>
+          <tr>
+            <td className="has-text-weight-bold">
+              Post Logout Redirect URIs
           </td>
-          <td>
-            {client.website_url}
-          </td>
-        </tr>
-        <tr>
-          <td className="has-text-weight-bold">
-            Contact Email
-          </td>
-          <td>
-            {client.contact_email}
-          </td>
-        </tr>
-        <tr>
-          <td className="has-text-weight-bold">
-            Logo
-          </td>
-          <td>
-            {client.logo}
-          </td>
-        </tr>
-        <tr>
-          <td className="has-text-weight-bold">
-            Reuse Content
-          </td>
-          <td>
-            {client.reuse_content}
-          </td>
-        </tr>
-        <tr>
-          <td className="has-text-weight-bold">
-            Redirect URIs
-          </td>
-          <td>
-            {client.redirect_uris}
-          </td>
-        </tr>
-        <tr>
-          <td className="has-text-weight-bold">
-            Post Logout Redirect URIs
-          </td>
-          <td>
-            {client.post_logout_redirect_uris}
-          </td>
-        </tr>
+            <td>
+              {client.post_logout_redirect_uris}
+            </td>
+          </tr>
+        </tbody>
       </table>
     </section>
   )
