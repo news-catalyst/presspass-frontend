6435fd1183780003f37bcc85d28c91db0caa0956
R. Miles McCain <x1-key@sendmiles.email>
2019-11-07 14:58:50 -0500

    Add loading placeholder

---
+++ src/common/loading.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+export const LoadingPlaceholder = () => {
+    return (
+        <p className="has-text-grey">Loading...</p>
+        // TODO: make this a nice loading animation.
+    )
+}
