291d8b16bf77b70feff67ec5f94994c65768f394
R. Miles McCain <x1-key@sendmiles.email>
2019-11-08 13:11:36 -0500

    Redirect on form save

--- src/clients/ClientEditPage.tsx
+++ src/clients/ClientEditPage.tsx
@@ -1,9 +1,10 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { AppActions } from '../store';
 import { ClientState, Client } from '../store/clients/types';
 import { ensureClients } from '../store/clients/api';
 import { LoadingPlaceholder } from '../common/loading';
 import ClientForm from './ClientForm';
+import { Redirect } from 'react-router';
 
 interface ClientPageProps {
   actions: AppActions;
@@ -25,20 +26,26 @@ const ClientPage = (props: ClientPageProps) => {
 
 const HydratedClientPage = (props: ClientPageProps) => {
   let client = props.clients.clients[props.client];
+  let [saved, setSaved] = useState(false);
 
   const handleSubmit = (updatedClient: Client) => {
     console.log(updatedClient);
     props.actions.upsertClient(updatedClient);
+    setSaved(true);
   };
 
-  return (
-    <section className="client-page">
-      <p className="subtitle">Edit OpenID Client</p>
-      <h1 className="title is-size-1">{client.name}</h1>
-        <ClientForm client={client} onSubmit={handleSubmit} />
-        <br />
-    </section>
-  )
+  if (saved) {
+    return <Redirect to={`/clients/${client.id}`} />
+  } else {
+    return (
+      <section className="client-page">
+        <p className="subtitle">Edit OpenID Client</p>
+        <h1 className="title is-size-1">{client.name}</h1>
+          <ClientForm client={client} onSubmit={handleSubmit} />
+          <br />
+      </section>
+    )
+  }
 }
 
 export default ClientPage;
