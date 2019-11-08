25efaec2629a07cbbfe2ab94dae013dffcc669af
R. Miles McCain <x1-key@sendmiles.email>
2019-11-08 15:27:22 -0500

    Code cleanup & create client API function

--- src/store/clients/api.ts
+++ src/store/clients/api.ts
@@ -2,15 +2,40 @@ import { AppActions } from '..';
 import { checkAuth, cfetch } from '../../utils';
 import { Client, ClientState } from './types';
 
-const HEADERS = {
+const REQ_BASE: RequestInit = {
   headers: {
     'Content-Type': 'application/json',
   },
+  credentials: 'include'
 };
 
-const GET = Object.assign({}, HEADERS, { method: 'GET', credentials: 'include' });
-const POST = Object.assign({}, HEADERS, { method: 'POST', credentials: 'include' });
-const PATCH = Object.assign({}, HEADERS, { method: 'PATCH', credentials: 'include' });
+const GET = Object.assign({}, REQ_BASE, { method: 'GET' });
+const POST = (body: any): RequestInit => ({
+  ...REQ_BASE,
+  method: 'POST',
+  body: body
+});
+const PATCH = (body: any): RequestInit => ({
+  ...REQ_BASE,
+  method: 'PATCH',
+  body: body
+});
+
+const serializeClient = (client: Client) => JSON.stringify({
+  id: client.id,
+  name: client.name || "",
+  client_type: client.client_type,
+  website_url: client.website_url || "",
+  terms_url: client.terms_url || "",
+  contact_email: client.contact_email || "",
+  // logo: client.logo || "", // TODO: pair on file uploading
+  reuse_consent: client.reuse_consent,
+  redirect_uris: client.redirect_uris || "",
+  post_logout_redirect_uris: client.post_logout_redirect_uris || "",
+  // Explicitly setting each field is necessary here, because
+  // not all of the fields in client are write-available, and
+  // including them in the request would result in a 400 response.
+})
 
 export const fetchClients = (actions: AppActions) =>
   cfetch(`${process.env.REACT_APP_SQUARELET_API_URL}/clients/`, GET)
@@ -29,28 +54,15 @@ export const ensureClients = (actions: AppActions, clients: ClientState) => {
 }
 
 export const updateClient = (client: Client, actions: AppActions) =>
-  cfetch(`${process.env.REACT_APP_SQUARELET_API_URL}/clients/${client.id}/`, Object.assign({},
-    PATCH, {
-      body: JSON.stringify({
-        // One potential optimization here would be to diff the old and new clients
-        // and only include the fields that differ; after all, this is a PATCH
-        // request.
-        id: client.id,
-        name: client.name || "",
-        client_type: client.client_type,
-        website_url: client.website_url || "",
-        terms_url: client.terms_url || "",
-        contact_email: client.contact_email || "",
-        // logo: client.logo || "",
-        reuse_consent: client.reuse_consent,
-        redirect_uris: client.redirect_uris || "",
-        post_logout_redirect_uris: client.post_logout_redirect_uris || "",
-        // Explicitly setting each field is necessary here, because
-        // not all of the fields in client are write-available, and
-        // including them in the request would result in a 400 response.
-      })
-  }))
-  .then(checkAuth(actions))
-  // Here is potentially where returning some kind of validation data would happen
-  // (Here or in a catch.)
-  .then(() => actions.upsertClient(client));
+  cfetch(`${process.env.REACT_APP_SQUARELET_API_URL}/clients/${client.id}/`, PATCH(serializeClient(client)))
+    .then(checkAuth(actions))
+    // Here is potentially where returning some kind of validation data would happen
+    // (Here or in a catch.)
+    .then(() => actions.upsertClient(client));
+
+export const createClient = (client: Client, actions: AppActions) =>
+  cfetch(`${process.env.REACT_APP_SQUARELET_API_URL}/clients/`, POST(serializeClient(client)))
+    .then(checkAuth(actions))
+    .then(() => fetchClients(actions));
+    // TODO: check if this is the most efficient way to update the store
+    // from both a code readability and network perspective
