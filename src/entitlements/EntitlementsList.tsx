3026f8b0024b4f2dbf906182551bf1b94938a4b2
Jacqui Lough <lough.jacqui@gmail.com>
2020-03-23 11:57:59 +1100

    Added a key to entitlement div (code cleanup)

--- src/entitlements/EntitlementsList.tsx
+++ src/entitlements/EntitlementsList.tsx
@@ -1,31 +1,35 @@
-import React, { useEffect } from "react";
+import React, { useEffect } from 'react';
 import { AppActions } from '../store';
-import { ensureEntitlements } from "../store/entitlements/api";
-import EntitlementCard from "./EntitlementCard";
-import { EntitlementState, Entitlement } from "../store/entitlements/types";
+import { ensureEntitlements } from '../store/entitlements/api';
+import EntitlementCard from './EntitlementCard';
+import { EntitlementState, Entitlement } from '../store/entitlements/types';
 
 interface EntitlementsListProps {
   actions: AppActions;
   entitlements: EntitlementState;
 }
 
-const EntitlementsList: React.FC<EntitlementsListProps> = (props: EntitlementsListProps) => {
+const EntitlementsList: React.FC<EntitlementsListProps> = (
+  props: EntitlementsListProps
+) => {
   useEffect(() => {
     ensureEntitlements(props.actions, props.entitlements);
   }, [props.actions, props.entitlements]);
 
   return (
     <div className="entitlements">
-    <h1 className="title is-size-1">Entitlements</h1>
-    <div className="columns is-multiline">
-    {Object.values(props.entitlements.entitlements).map((entitlement: Entitlement) => (
-      <div className="column is-4">
-        <EntitlementCard entitlement={entitlement} />
+      <h1 className="title is-size-1">Entitlements</h1>
+      <div className="columns is-multiline">
+        {Object.values(props.entitlements.entitlements).map(
+          (entitlement: Entitlement) => (
+            <div key={entitlement.id} className="column is-4">
+              <EntitlementCard entitlement={entitlement} />
+            </div>
+          )
+        )}
       </div>
-    ))}
     </div>
-    </div>
-  )
-}
+  );
+};
 
 export default EntitlementsList;
