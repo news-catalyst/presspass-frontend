020264a5ff55fcf478180bc8a38a054b9b3e9e42
Jacqui Lough <lough.jacqui@gmail.com>
2020-04-29 10:39:05 +1000

    Fixed up layout, added in title to pitch

--- src/common/ToolbuilderPitch.tsx
+++ src/common/ToolbuilderPitch.tsx
@@ -8,10 +8,15 @@ const ToolbuilderPitch: React.FC<ToolbuilderProps> = (
   props: ToolbuilderProps
 ) => {
   return (
-    <>
-      <p>{props.archie.copy.toolbuilder.description}</p>
-      <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSc3f7KZ0MennmLIPHuIGKvPmj3TvQ2p0n3H0jEBt4i_pEn7-Q/viewform?embedded=true" width="640" height="1003">Loading…</iframe>
-    </>
+    <section className="section">
+      <div className="container">
+        <div className="content">
+          <h1 className="title is-size-1">{props.archie.copy.toolbuilder.title}</h1>
+          <p>{props.archie.copy.toolbuilder.description}</p>
+          <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSc3f7KZ0MennmLIPHuIGKvPmj3TvQ2p0n3H0jEBt4i_pEn7-Q/viewform?embedded=true" width="640" height="1003">Loading…</iframe>
+        </div>
+      </div>
+    </section>
   );
 };
 
